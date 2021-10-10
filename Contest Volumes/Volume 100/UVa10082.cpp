a6d1d9e52a2030be7403c7fe5a28bdf332e823ce
rokashou <keima.sawamura@gmail.com>
2021-10-10 22:37:06 +0900

    劉汝佳版最佳解

---
+++ Contest Volumes/Volume 100/UVa10082.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+char s[] = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
+int main()
+{
+    int i,c;
+    while((c = getchar())!=EOF){
+        for (i=1;s[i] && s[i]!=c;i++); // found the pos in table.
+        if(s[i]) putchar(s[i-1]);
+        else putchar(c);
+    }
+    return 0;
+}
